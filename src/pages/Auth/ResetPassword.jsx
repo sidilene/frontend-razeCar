8772465fcf967b172160178dbf7f163031f270a5
Sidilene <sidilenedocarmo751@gmail.com>
2026-04-07 18:38:07 -0300

    Desfazendo a correcao da  regra do isLocalhost para IP do iPhone

--- src/pages/Auth/ResetPassword.jsx
+++ src/pages/Auth/ResetPassword.jsx
@@ -19,12 +19,7 @@ export default function ResetPassword() {
   // 👇 ESTADO NOVO: Detecta se é celular
   const [isMobile, setIsMobile] = useState(false);
 
-  const hostname = window.location.hostname;
-
-  // Verifica se é localhost, 127.0.0.1 OU um IP da sua rede local (começando com 192.168.)
-  const isLocalhost = hostname === "localhost" ||
-                      hostname === "127.0.0.1" ||
-                      hostname.startsWith("192.168.");
+  const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
 
   // O link do Expo com seu IP atual
   const deepLinkApp = isLocalhost
